a4880c17c86fb457467f788deaef47752ccfdbb8
Wresni Ronggowerdhi <wresni.ronggowerdhi@indivaragroup.com>
2025-09-18 17:05:37 +0700

    initiate lib

---
+++ src/test/java/dev/wresni/common/SpringCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wresni.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
